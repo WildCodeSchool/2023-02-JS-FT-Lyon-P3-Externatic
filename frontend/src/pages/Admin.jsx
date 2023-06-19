c2804cb8974aa1e5004685bb9d03f33e5a881c4f
Ropie1981 <pierre.saumont@gmail.com>
2023-06-19 14:26:41 +0200

    navbar css fix

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Navbar from "../components/Navbar";
 import AdminCreate from "../components/AdminCreate";
 import CandidatesTable from "../components/CandidatesTable";
 import CompaniesTable from "../components/CompaniesTable";
 
 export default function Admin() {
   return (
-    <Container maxWidth="lg">
-      <Typography variant="h2" color="initial">
-        Admin Page
-      </Typography>
-      <AdminCreate />
-      <CandidatesTable />
-      <CompaniesTable />
-    </Container>
+    <>
+      <Navbar />
+      <Container maxWidth="lg">
+        <Typography variant="h2" color="initial">
+          Admin Page
+        </Typography>
+        <AdminCreate />
+        <CandidatesTable />
+        <CompaniesTable />
+      </Container>
+    </>
   );
 }
