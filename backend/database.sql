9196940bfa216ee0647dd9cec8215fb1fda5e996
Ropie1981 <pierre.saumont@gmail.com>
2023-07-04 17:10:42 +0200

    adding ADMIN in DB

--- backend/database.sql
+++ backend/database.sql
@@ -73,7 +73,7 @@ ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
 INSERT INTO user (email, phone, city, picture, hashedPassword, admin)
 VALUES
-  ('john.doe@example.com', '1234567890', 'New York', NULL, '$argon2id$v=19$m=19456,t=2,p=1$vvakCosUkyOkLzeryb3Ahg$8mfwLBB4Wqv7sDLSoRDSuKqADfnC/cSVo8wHvOnLdLI', 0),
+  ('admin@mail.com', '1234567890', 'Admin City', NULL, '$argon2id$v=19$m=19456,t=2,p=1$OHrIYr3oh1aV8bMvuQQfVg$ulKfOEOlASVNSqHrFtqRQyxUu85biaS/jmsjTH203iE', 1),
   ('jane.smith@example.com', '9876543210', 'Los Angeles', NULL, '$argon2id$v=19$m=19456,t=2,p=1$vvakCosUkyOkLzeryb3Ahg$8mfwLBB4Wqv7sDLSoRDSuKqADfnC/cSVo8wHvOnLdLI', 0),
   ('michael.brown@example.com', '5555555555', 'Chicago', NULL, '$argon2id$v=19$m=19456,t=2,p=1$vvakCosUkyOkLzeryb3Ahg$8mfwLBB4Wqv7sDLSoRDSuKqADfnC/cSVo8wHvOnLdLI', 0),
   ('emily.johnson@example.com', '9999999999', 'San Francisco', NULL, '$argon2id$v=19$m=19456,t=2,p=1$vvakCosUkyOkLzeryb3Ahg$8mfwLBB4Wqv7sDLSoRDSuKqADfnC/cSVo8wHvOnLdLI', 0),
@@ -101,7 +101,7 @@ VALUES
 
 INSERT INTO candidate (user_id, firstname, lastname, cv)
 VALUES
-  (1, 'John', 'Doe', NULL),
+  (1, 'Admin', 'Test', NULL),
   (2, 'Jane', 'Smith', NULL),
   (3, 'Michael', 'Brown', NULL),
   (4, 'Emily', 'Johnson', NULL),
@@ -144,7 +144,7 @@ VALUES
   (11, 'Financial Advisor', 'Provide financial planning and investment advice', 'Bachelor\'s degree in Finance, relevant certifications (e.g., CFP)', 'CDI', 'Teletravail', 'Dallas', 'USD 100,000', '2023-06-11', 0),
   (12, 'Travel Consultant', 'Plan and book travel arrangements for clients', 'High school diploma, knowledge of popular travel destinations', 'CDD', 'Presentiel', 'Phoenix', 'USD 50,000', '2023-06-12', 0);
 
-INSERT INTO applications (candidate_id, job_posting_id, date, status)
+INSERT INTO application (candidate_id, job_posting_id, date, status)
 VALUES
   (1, 1, '2023-06-19', 'en cours'),
   (2, 2, '2023-06-18', 'rejetée'),
