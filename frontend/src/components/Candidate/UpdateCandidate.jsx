674a2c86e68eb8688a7243c320e821ded48843e7
Marcelo da Silva <marceloxhenrique@hotmail.com>
2023-07-15 15:13:31 +0200

    set up validate form in candidate update data

--- frontend/src/components/Candidate/UpdateCandidate.jsx
+++ frontend/src/components/Candidate/UpdateCandidate.jsx
@@ -9,12 +9,14 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Container from "@mui/material/Container";
 import CandidateContext from "../../Contexts/CandidateContext";
+import { ValidateFormUpdateCandidate } from "../ValidateForm";
 
 function UpdateCandidate() {
   const navigate = useNavigate();
   const notifyCreation = () => toast("Votre compte a bien été modifié !");
 
   const { candidate } = useContext(CandidateContext);
+  const [validateInput, setValidateInput] = useState({});
 
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -27,19 +29,32 @@ function UpdateCandidate() {
     email: `${candidate.email}`,
   });
 
-  const validateForm = () => {
-    // add email Validation
-    return true;
-  };
-
   const handleInputChange = (event) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
+  const validateForm = () => {
+    const { error } = ValidateFormUpdateCandidate.validate(
+      { ...formData, terms: undefined },
+      {
+        abortEarly: false,
+        allowUnknown: true,
+      }
+    );
+    if (error) {
+      const validationErrors = {};
+      error.details.forEach((err) => {
+        validationErrors[err.context.key] = err.message;
+      });
+      return validationErrors;
+    }
+    return {};
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    if (validateForm) {
+    const validationErrors = validateForm();
+    if (Object.keys(validationErrors).length === 0) {
       axios
         .put(`${BACKEND_URL}/candidates/${candidate.id}`, { ...formData })
         .then(() => {
@@ -51,8 +66,16 @@ function UpdateCandidate() {
         .catch((err) => {
           console.error(err);
         });
+    } else {
+      // The form is invalid, handle validation errors
+      setValidateInput({ ...validationErrors });
+      console.error("Validation Errors:", validationErrors);
     }
   };
+
+  React.useEffect(() => {
+    validateForm();
+  }, [handleSubmit, handleInputChange]);
 
   return (
     <Container maxWidth="md">
@@ -61,7 +84,10 @@ function UpdateCandidate() {
           component="form"
           noValidate
           onSubmit={handleSubmit}
-          sx={{ mt: 3, mb: 2 }}
+          sx={{
+            mt: 3,
+            mb: 2,
+          }}
         >
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
@@ -76,6 +102,14 @@ function UpdateCandidate() {
                 onChange={handleInputChange}
                 value={formData.firstname}
               />
+              <Box
+                color="primary.main"
+                sx={{
+                  textAlign: "left",
+                }}
+              >
+                {formData.firstname ? undefined : validateInput.firstname}
+              </Box>
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -88,6 +122,14 @@ function UpdateCandidate() {
                 onChange={handleInputChange}
                 value={formData.lastname}
               />
+              <Box
+                color="primary.main"
+                sx={{
+                  textAlign: "left",
+                }}
+              >
+                {formData.lastname ? undefined : validateInput.lastname}
+              </Box>
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -100,6 +142,14 @@ function UpdateCandidate() {
                 onChange={handleInputChange}
                 value={formData.city}
               />
+              <Box
+                color="primary.main"
+                sx={{
+                  textAlign: "left",
+                }}
+              >
+                {formData.city ? undefined : validateInput.city}
+              </Box>
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -112,6 +162,14 @@ function UpdateCandidate() {
                 onChange={handleInputChange}
                 value={formData.phone}
               />
+              <Box
+                color="primary.main"
+                sx={{
+                  textAlign: "left",
+                }}
+              >
+                {validateInput.phone}
+              </Box>
             </Grid>
             <Grid item xs={12}>
               <TextField
@@ -124,6 +182,14 @@ function UpdateCandidate() {
                 onChange={handleInputChange}
                 value={formData.email}
               />
+              <Box
+                color="primary.main"
+                sx={{
+                  textAlign: "left",
+                }}
+              >
+                {validateInput.email}
+              </Box>
             </Grid>
           </Grid>
           <Button
--- frontend/src/components/ValidateForm.jsx
+++ frontend/src/components/ValidateForm.jsx
@@ -45,11 +45,11 @@ const ValidateFormCandidate = Joi.object({
     .required(),
   password: Joi.string()
     .min(6)
-    .max(20)
+    .max(30)
     .messages({
       "string.empty": "Le mot de passe est requis",
       "string.min": "Le mot de passe doit contenir au moins 6 caractères",
-      "string.max": "Le mot de passe ne peut pas dépasser 20 caractères",
+      "string.max": "Le mot de passe ne peut pas dépasser 30 caractères",
       "any.required": "Le mot de passe est requis",
     })
     .required(),
@@ -117,14 +117,63 @@ const ValidateFormCompany = Joi.object({
     .required(),
   password: Joi.string()
     .min(6)
-    .max(20)
+    .max(30)
     .messages({
       "string.empty": "Le mot de passe est requis",
       "string.min": "Le mot de passe doit contenir au moins 6 caractères",
-      "string.max": "Le mot de passe ne peut pas dépasser 20 caractères",
+      "string.max": "Le mot de passe ne peut pas dépasser 30 caractères",
       "any.required": "Le mot de passe est requis",
     })
     .required(),
 });
 
-export { ValidateFormCandidate, ValidateFormCompany };
+const ValidateFormUpdateCandidate = Joi.object({
+  firstname: Joi.string()
+    .min(1)
+    .messages({
+      "string.empty": "Le prénom est requis",
+      "string.min": "Le prénom doit contenir au moins 1 caractère",
+      "any.required": "Le prénom est requis",
+    })
+    .required(),
+  lastname: Joi.string()
+    .min(1)
+    .messages({
+      "string.empty": "Le nom est requis",
+      "string.min": "Le nom doit contenir au moins 1 caractère",
+      "any.required": "Le nom est requis",
+    })
+    .required(),
+  phone: Joi.string()
+    .pattern(/^[0-9]+$/)
+    .min(9)
+    .messages({
+      "string.empty": "Le numéro de téléphone est requis",
+      "string.pattern.base": "Le téléphone doit être un numéro valide",
+      "string.min": "Le téléphone doit être un numéro valide",
+      "any.required": "Le téléphone doit être un numéro valide",
+    })
+    .required(),
+  city: Joi.string()
+    .min(1)
+    .messages({
+      "string.empty": "Le nom de la ville est requis",
+      "string.min": "Le nome de la ville doit contenir au moins 1 caractére",
+      "any.required": "Le nom de la ville est requis",
+    })
+    .required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .messages({
+      "string.empty": "Email est requis",
+      "string.email": "Email doit être valid",
+      "any.required": "Email doit être valid",
+    })
+    .required(),
+});
+
+export {
+  ValidateFormCandidate,
+  ValidateFormCompany,
+  ValidateFormUpdateCandidate,
+};
